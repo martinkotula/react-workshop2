43f8e6b40bab5ac8be8b41e139a1da9d6544b11c
Martin Kotula <marcin.kotula.ctr@sabre.com>
2017-08-25 09:44:08 +0200

    internationalization helpers

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import App from './components/App';
 //configure react-intl
 import { addLocaleData } from 'react-intl';
 import en from 'react-intl/locale-data/en';
-import pl from 'react-intl/locale-data/pl'
+import pl from 'react-intl/locale-data/pl'; //source of date/number formatting
 
 import { IntlProvider } from 'react-intl';
 import translations from './translations';
 
 addLocaleData([...en, ...pl]);
 
-const locale = "en";
+const locale = "pl";
 
 ReactDOM.render(
 <IntlProvider locale={locale} messages={translations[locale]}>
--- src/views/Main.js
+++ src/views/Main.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
-import {FormattedMessage} from 'react-intl';
+import {FormattedMessage, FormattedDate, FormattedRelative, FormattedTime, FormattedPlural, FormattedNumber  } from 'react-intl';
 
 import Link from '../components/Link';
 
 
 class Main extends Component{
+    constructor(){
+        super();
 
+        this.state = {
+            amount: 10
+        }
+    }
     render(){
         return (
             <div className="main">
@@ -15,6 +21,35 @@ class Main extends Component{
                     defaultMessage={"Welcome from {name}!"}
                     values={{name:"React"}}
                 />
+                
+                <br />
+
+                <FormattedDate value={Date.now()} />
+
+                <br />
+
+                <FormattedRelative value={'2017-08-21 20:00'} />
+
+                <br />
+                
+                <FormattedNumber value={'22.40'} />
+
+                <br />
+                
+                <FormattedTime value={'2017-08-21 20:00'} />
+
+                <br />
+                {this.state.amount}{' '}
+                <FormattedPlural 
+                    value={this.state.amount}
+                    one="pieróg"
+                    few="pierogi"
+                    many="pierogów"
+                    other="pieroga"
+                />
+
+                <br />
+
                 <Link path="/details">Go to details</Link>
             </div>
         );
