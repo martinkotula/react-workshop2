8bb63ce74c746c26127f271c2afbd3a5fc6571e6
Martin Kotula <marcin.kotula.ctr@sabre.com>
2017-08-25 09:13:15 +0200

    Usage of react-intl to provide translations

--- src/index.js
+++ src/index.js
@@ -2,4 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+//configure react-intl
+import { addLocaleData } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import pl from 'react-intl/locale-data/pl'
+
+import { IntlProvider } from 'react-intl';
+import translations from './translations';
+
+addLocaleData([...en, ...pl]);
+
+const locale = "en";
+
+ReactDOM.render(
+<IntlProvider locale={locale} messages={translations[locale]}>
+    <App />
+</IntlProvider>, document.getElementById('root'));
--- src/views/Main.js
+++ src/views/Main.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
+import {FormattedMessage} from 'react-intl';
+
 import Link from '../components/Link';
+
 
 class Main extends Component{
 
     render(){
         return (
             <div className="main">
-                Main
+                <FormattedMessage
+                    id="Main.first-message"
+                    description="Welcome message used on main screen"
+                    defaultMessage={"Welcome from {name}!"}
+                    values={{name:"React"}}
+                />
                 <Link path="/details">Go to details</Link>
             </div>
         );
